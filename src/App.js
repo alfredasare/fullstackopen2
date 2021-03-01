91280155c3d8bc9548e351c4dcef88f6be68aa61
alfredasare <alfredasare101@gmail.com>
2021-03-01 22:52:58 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({text, handleClick}) => (
+    <button onClick={handleClick}>
+        {text}
+    </button>
+);
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [counter, setCounter] = useState(0);
+
+    const increaseByOne = () => setCounter(counter + 1)
+
+    const setToZero = () => setCounter(0)
+
+    const decreaseByOne = () => setCounter(counter - 1);
+
+    return (
+        <div>
+            <Display counter={counter} />
+            <Button handleClick={increaseByOne} text='plus'/>
+            <Button handleClick={setToZero} text='zero' />
+            <Button handleClick={decreaseByOne} text='minus' />
+        </div>
+    );
 }
 
 export default App;
