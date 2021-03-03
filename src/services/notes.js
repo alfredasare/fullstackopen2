d60469b2d749afab97462133974d140b46b14fce
alfredasare <alfredasare101@gmail.com>
2021-03-03 21:33:37 +0000

    Build for heroku

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'https://mighty-mountain-16496.herokuapp.com/api/notes';
+const baseUrl = '/api/notes';
 
 const getAll = () => {
     const request = axios.get(baseUrl);
